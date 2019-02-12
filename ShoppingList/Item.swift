15aaea19eeb3c0b760e774f3c4b40f47adbc47d3
Joel Chamakala <jchamakala9074@jh-mba158-04.local>
2019-02-12 11:28:09 -0600

    Creating Item on a new swift file

---
+++ ShoppingList/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  ShoppingList
+//
+//  Created by Joel Chamakala on 2/12/19.
+//  Copyright © 2019 Joel Chamakala. All rights reserved.
+//
+
+import Foundation
+class Item{
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
