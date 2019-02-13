98461142f8851926fd325a0d7c6ebb3d0bede2ee
Joel Chamakala <jchamakala9074@jh-mba158-04.local>
2019-02-13 11:26:20 -0600

    Added the add funtionallity for new items

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -11,22 +11,34 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var newItemTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
+    var items: [Item] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         
+        let item1 = Item(name: "Milk")
+        let item2 = Item(name: "Eggs")
+        items = [item1, item2]
+        let item3 = Item(name: "Bread")
+        items.append(item3)
+
     }
 
     @IBAction func addNewItemButtonPressed(_ sender: Any) {
-        
+        if let newItemName = newItemTextField.text{
+            let newItem = Item(name: newItemName)
+            items.append(newItem)
+            tableView.reloadData()
+        }
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
-        cell.textLabel?.text = "Hello"
+        let currentItemName = items[indexPath.row].name
+        cell.textLabel?.text = currentItemName
         return cell
     }
     
